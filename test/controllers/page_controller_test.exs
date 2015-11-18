b1e974c16d661176c633b9095923912666a98e3f
Diego Echeverri <diego.echeverri@wooga.com>
2015-11-18 10:08:37 -0500

    Fixing test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Backend.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Break-Kraut.swf"
   end
 end
